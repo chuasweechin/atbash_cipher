1fdb684bfca6870a2e2fc5dc529ef28b70eaa8fd
chuasweechin <sampiochua@gmail.com>
2019-03-14 09:53:59 +0800

    upload solution

---
+++ script.js
@@ -0,0 +1,56 @@
+var cipher = {};
+
+var firstWord = "hello";
+var firstWordArray = firstWord.split("");
+
+var secondWord = "peter";
+var secondWordArray = secondWord.split("");
+
+var encodedWord = "";
+
+var firstEncodedWord = "svool";
+var firstEncodedWordArray = firstEncodedWord.split("");
+
+var decodedWord = "";
+
+function createFoldingCipher (input) {
+    var inputArray = input.split("");
+    console.log(inputArray);
+    for (var a = 0; a < inputArray.length; a++) {
+        inputFromReverseDirection = inputArray[(inputArray.length - 1)  -  a]
+        cipher[inputArray[a]] =  inputFromReverseDirection;
+    }
+}
+
+function encode (wordArray) {
+    for (var a = 0; a < wordArray.length; a++) {
+        for (var code in cipher) {
+            if (wordArray[a] === code) {
+                encodedWord = encodedWord + cipher[code];
+            }
+        }
+    }
+    console.log(encodedWord);
+}
+
+function decode (wordArray) {
+    for (var a = 0; a < wordArray.length; a++) {
+        for (var code in cipher) {
+            if (wordArray[a] === cipher[code]) {
+                decodedWord = decodedWord + code;
+            }
+        }
+    }
+    console.log(decodedWord);
+}
+
+
+createFoldingCipher ("abcdefghijklmnopqrstuvwxyz");
+
+var userInput = prompt("Input a word to be encoded");
+var userInputArray = userInput.split("");
+
+encode(userInputArray);
+
+encode(firstWordArray);
+decode(firstEncodedWordArray);
